ec5cd32a60c19953e2f2d9d1d3fea1cc62a3c9be
Hendry Zhou <hendryzhou889@gmail.com>
2021-01-08 23:11:41 -0500

    clean up

--- food/lib/screens/home/components/body.dart
+++ food/lib/screens/home/components/body.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:food/components/search_bar.dart';
-import 'package:food/constants.dart';
 import 'package:food/screens/home/components/category_list.dart';
 import 'package:food/screens/home/components/discount_card.dart';
 import 'package:food/screens/home/components/item_list.dart';
